9dab3b4d5f7ac8c71587a06c04f79d5582e31c60
OPNA2608 (Christoph Neidahl) <christoph.neidahl@uni-leipzig.de>
2019-04-15 12:34:54 +0200

    Add execution trace to travis script
    ...so script progression can be seen in the build log,
    in case any errors appear.

--- .travis/beforeInstall.sh
+++ .travis/beforeInstall.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+echo "OS: ""$TRAVIS_OS_NAME"
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
-  
+
+  set -o xtrace
   # macOS: install Qt5 per brew
   brew install qt5
   brew link --force qt5
   
 elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
-  
+
+  set -o xtrace
   # Linux: add PPA, download Qt5 per apt
   sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
   sudo apt update -qq
@@ -17,7 +20,7 @@ elif [ "$TRAVIS_OS_NAME" == "linux" ]; then
 else
   
   # Not-yet-added OS, can't know how to get Qt5 setup
-  echo "Unknown OS name ""$TRAVIS_OS_NAME"", don't know how to acquire Qt5!" 1>&2
+  echo "Unknown OS name '""$TRAVIS_OS_NAME""', don't know how to acquire Qt5!" 1>&2
   echo "Please add instructions to '.travis/beforeInstall.sh'!" 1>&2
   exit 1
   
